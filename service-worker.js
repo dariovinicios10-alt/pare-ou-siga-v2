5bdb037131b34ea0067a62186b1f16d58b4cd342
dariovinicios10-alt <dariovinicios10@gmail.com>
2026-06-22 08:01:24 -0400

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    Estratégia: cache-first com atualização em segundo plano.
    ===================================================================== */
 
-const CACHE = "pare-ou-siga-v5";
+const CACHE = "pare-ou-siga-v6";
 
 const ARQUIVOS = [
   "./",
